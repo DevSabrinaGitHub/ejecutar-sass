43db5784b5a193eb3031d0495dc07fd94a9c2822
Dev Sabrina <155199050+DevSabrinaGitHub@users.noreply.github.com>
2024-01-30 10:39:32 -0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@
 //src sirve para identificar un archivo
 //dest sirve para guardarlo
 //requiere gulp, busca el archivo gulp en node_modules y aplica al archivo gulpfile
-const{src, dest}= require("gulp");
-const sass= require('sass'); //importar archivo sass para poder ejecutarlo
+//agregamos un watch 
+const{src, dest, watch}= require("gulp");
+const sass= require('gulp-sass') (require('sass')); //importar archivo sass para poder ejecutarlo
 
 
 function css(done){
@@ -21,4 +22,11 @@ function css(done){
 
 }
 
+function dev(done){
+
+    watch("src/scss/app.scss", css);
+    done();
+}
+//guardo el archivo como .dev e asigno lo que tiene la funcion dev
 exports.css= css;
+exports.dev= dev;
